6f7c64182a67cc05b4ff790d93b2c1f0ca0f11f2
Alfredo Leonelli <alfredoleonellim@gmail.com>
2022-06-28 17:56:14 -0400

    Funciones actualizadas

--- WebServices/select lugar.php
+++ WebServices/select lugar.php
@@ -1,5 +1,5 @@
 <?php
-    function getLugaresOptions($lugar_id) {
+    function getDestinoOptions($lugar_id) {
         require 'database.php';
         $consulta = "select * from LUGARES";
         $resultado = mysqli_prepare($conexion, $consulta);
@@ -19,6 +19,25 @@
                 } else {
                     echo "<option value='$r_id'>$r_lugar</option>";
                 }
+            }
+        }
+    }
+    function getOrigenOptions($piso_usuario) {
+        require 'database.php';
+        $consulta = "select * from LUGARES where ".$piso_usuario." = ID";
+        $resultado = mysqli_prepare($conexion, $consulta);
+
+        if(!$resultado) {
+            echo "Error: ".mysqli_error($conexion);
+        }
+        $ok = mysqli_stmt_execute($resultado);
+
+        if(!$ok) {
+            echo "Error";
+        } else {
+            $ok = mysqli_stmt_bind_result($resultado, $r_id, $r_lugar);
+            while($fila = mysqli_stmt_fetch($resultado)) {
+                echo "<option value='$r_id' selected>$r_lugar</option>";
             }
         }
     }
