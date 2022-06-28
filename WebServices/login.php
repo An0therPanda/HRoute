da5e3efbacbbbf2af7f2e103d4e652d1e7ef1611
Alfredo Leonelli <alfredoleonellim@gmail.com>
2022-06-28 17:56:23 -0400

    Session actualizado

--- WebServices/login.php
+++ WebServices/login.php
@@ -7,7 +7,7 @@
         $usuario = $_POST['usuario'];
         $passwd = $_POST['passwd'];
 
-        $consulta = "select id, usuario, contrasena, tipo_usuario from usuarios where usuario = ? and contrasena = ?";
+        $consulta = "select id, usuario, contrasena, tipo_usuario, piso from usuarios where usuario = ? and contrasena = ?";
 
         $resultado = mysqli_prepare($conexion, $consulta);
         
@@ -15,7 +15,7 @@
 
         $ok = mysqli_stmt_bind_param($resultado,"ss", $usuario, $passwd);
         $ok = mysqli_stmt_execute($resultado);
-        $ok = mysqli_stmt_bind_result($resultado, $r_id, $r_usuario, $r_contra, $r_tipo);
+        $ok = mysqli_stmt_bind_result($resultado, $r_id, $r_usuario, $r_contra, $r_tipo, $r_piso);
 
         if(!mysqli_stmt_fetch($resultado)){
             $_SESSION['mensaje'] = "Login Fallido, Usuario no encontrado";
@@ -27,6 +27,7 @@
 
             $_SESSION['id'] = $r_id;
             $_SESSION['tipo'] = $r_tipo;
+            $_SESSION['piso'] = $r_piso;
             if($r_tipo == 1){
                 header('location: ../HRoute/admin/crear.php');
             }else if($r_tipo == 2){
