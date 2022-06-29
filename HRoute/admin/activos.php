0f960808deccad8ecb46f62d93e8f678c822b446
Benjamín Álvarez <benjaex2@gmail.com>
2022-06-29 18:18:21 -0400

    errores solucionados

--- HRoute/admin/activos.php
+++ HRoute/admin/activos.php
@@ -5,7 +5,7 @@ if (isset($_SESSION["tipo"])) {
     header('location: ../asistente/trasladospendientes.php');
   }
   if ($_SESSION["tipo"] == 3) {
-    header('location: ../enfer/crear.php');
+    header('location: ../enfer/agregar.php');
   }
 } else {
   header('location: ../index.php');
